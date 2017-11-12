36303a82c5a3227efdfebac0a7c0dce156e08e58
Raymond Rupiah <rupiray@yahoo.com>
2017-11-12 00:10:58 +0000

    modified files

--- myhellowebapp/hellowebapp/urls.py
+++ myhellowebapp/hellowebapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about' ),
     url(r'^services/$', TemplateView.as_view(template_name='services.html'), name='services' ),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact' ),
-
+    url(r'^profiles/(?P<slug>[-\w]+)/$', views.profile_detail, name='profile_detail'),
+    url(r'^profiles/(?P<slug>[-\w]+)/edit/$', views.edit_profile, name='edit_profile'),
     url(r'^admin/', admin.site.urls),
 ]
